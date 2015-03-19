bf8dca2c71e5d6d28ccbd8a4432ec73e1ae9a86e
Jay Wengrow <jaywngrw@gmail.com>
2015-03-18 20:20:41 -0500

    added user and api key authorization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310231748) do
+ActiveRecord::Schema.define(version: 20150319005340) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "address_1",   limit: 255
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20150310231748) do
     t.integer  "employee_id", limit: 4
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      limit: 255
+    t.string   "api_key",    limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
